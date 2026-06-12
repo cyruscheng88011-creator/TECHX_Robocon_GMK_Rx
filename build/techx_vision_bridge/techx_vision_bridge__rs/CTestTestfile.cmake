4a43eacdcb073f5f745025f8a48e4fa16f0fe225
cyruscheng8811 <cyruscheng8811@users.noreply.github.com>
2026-06-13 03:28:58 +0800

    Initial commit: TECHX Robocon GMK Rx project

---
+++ build/techx_vision_bridge/techx_vision_bridge__rs/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/cyrus/robocon2026/GMK/build/techx_vision_bridge/techx_vision_bridge__rs
+# Build directory: /home/cyrus/robocon2026/GMK/build/techx_vision_bridge/techx_vision_bridge__rs
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
